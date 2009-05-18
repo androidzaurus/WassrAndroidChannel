c3ae9fe0466a03b8aae163f481d3bd453be10159
androidzaurus <androidzaurus@yahoo.co.jp>
2009-05-18 16:54:08 +0900

    add main view as ListActivity

--- src/net/seesaa/androidzaurus/android/wassrandroidchannel/AndroidChanView.java
+++ src/net/seesaa/androidzaurus/android/wassrandroidchannel/AndroidChanView.java
@@ -1,14 +1,19 @@
 package net.seesaa.androidzaurus.android.wassrandroidchannel;
 
 import net.seesaa.androidzaurus.android.wassrandroidchannel.WassrRss.WassrItems;
-
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.LiveFolders;
+import android.widget.SimpleCursorAdapter;
 
-public class AndroidChanView extends Activity {
+public class AndroidChanView extends ListActivity {
+	private static final String[] PROJECTION = new String [] {
+		LiveFolders._ID,
+		LiveFolders.DESCRIPTION,
+	};
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -19,11 +24,16 @@ public class AndroidChanView extends Activity {
 		if (LiveFolders.ACTION_CREATE_LIVE_FOLDER.equals(action)) {
 			setResult(RESULT_OK, createLiveFolder(this, WassrItems.LIVE_FOLDER_URI,
 					"Wassr Android Channel", R.drawable.wassricon));
+			finish();
 		} else {
-			setResult(RESULT_CANCELED);
+			if (intent.getData() == null) {
+				intent.setData(WassrItems.CONTENT_URI);
+			}
+			Cursor cursor = managedQuery(getIntent().getData(), PROJECTION, null, null, WassrItems.DEFAULT_SORT_ORDER);
+			SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.main, cursor, 
+					new String[] {LiveFolders.DESCRIPTION}, new int[] {R.id.TextView01});
+			setListAdapter(adapter);
 		}
-
-		finish();
 	}
 
 	private Intent createLiveFolder(AndroidChanView androidChanView,
